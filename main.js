34c6884eaa536a8059e29baa1023102967bf1658
kaitabuchi <kaidanieltabuchi@gmail.com>
2022-05-22 13:37:33 -0500

    Clickable Button Bar

---
+++ main.js
@@ -0,0 +1,22 @@
+if(document.getElementById('home').clicked == true)
+{
+  window.location.href = "index.html";
+}
+
+if(document.getElementById('channel').clicked == true)
+{
+  window.location.href = "https://www.youtube.com/channel/UC92UEMwg9Wf4Oe5_D9C0Xcg";
+}
+
+if(document.getElementById('aboutme').clicked == true)
+{
+  window.location.href = "aboutme.html";
+}
+
+if(document.getElementById('mygames').clicked == true)
+{
+  window.location.href = "https://xyzcoder.itch.io/";
+}
+
+//https://xyzcoder.itch.io/
+//https://www.youtube.com/channel/UC92UEMwg9Wf4Oe5_D9C0Xcg
